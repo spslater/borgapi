ebcbd235fb8ea46603f7c097f239ccf1bc9e8ac0
Sean Slater <seanslater@whatno.io>
2021-05-10 20:53:17 -0400

    Add Sample Files
    Add sample files to help with understanding use.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="borgapi",
-    version="0.1.1",
+    version="0.1.2-alpha.1",
     author="Sean Slater",
     author_email="seanslater@whatno.io",
     description="Wrapper for borgbackup to easily use in code",
